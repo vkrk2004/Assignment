442ad0580ee63888b76b7d0fb8c2230166d61826
vkrk2004 <varunkumarkudumula@gmail.com>
2024-06-17 06:33:22 +0530

    Update PersonalInfo.cs

--- PersonalInfo.cs
+++ PersonalInfo.cs
@@ -1,1 +1,33 @@
+using System;
 
+namespace PersonalInfoApp
+{
+    class PersonalInfo
+    {
+        static void Main(string[] args)
+        {
+
+            DisplayPersonalInfo("Lucinda Potter", "6/24/1992", "000-000-0101", "000-000-0189");
+            Console.WriteLine();
+
+
+            DisplayPersonalInfo("John Smith", "8/15/1978", "000-111-0101", "000-111-0189");
+            Console.WriteLine();
+
+
+            DisplayPersonalInfo("Chris Johnson", "12/01/1987", "000-222-0101", "000-222-0189");
+            Console.WriteLine();
+
+
+            DisplayPersonalInfo("Robert Hall", "2/27/1949", "000-444-0101", "000-444-0189");
+        }
+
+        static void DisplayPersonalInfo(string name, string birthdate, string workPhone, string cellPhone)
+        {
+            Console.WriteLine(name);
+            Console.WriteLine(birthdate);
+            Console.WriteLine("work " + workPhone);
+            Console.WriteLine("cell " + cellPhone);
+        }
+    }
+}
